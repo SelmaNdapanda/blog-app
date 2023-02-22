2a5358fa2ae70baa2507e89a7a98bb2a7ab2f452
Selma <selmandapanda@yahoo.com>
2023-02-22 18:35:56 +0200

    Create Custom Methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
   belongs_to :author, class_name: 'User'
+
+  def update_post_counter_in_user
+    users.post_counter.update
+  end
+
+  def most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
