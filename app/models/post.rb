132741d4331f9906c40727e75af62296470a26c6
Selma <selmandapanda@yahoo.com>
2023-02-22 17:26:27 +0200

    Set up associations between models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  has many :comments
+  has many :likes
+  belongs_to :author, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has many :posts, foreign_key: :'author_id'
+  has many :likes, foreign_key: :'author_id'
+  has many :comments, foreign_key: :'author_id'
 end
