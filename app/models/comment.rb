f5d2e82947766a337984fe52a0f0386d0880c6db
Selma <selmandapanda@yahoo.com>
2023-03-03 16:22:48 +0200

    Update models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  belongs_to :post
+  belongs_to :post, class_name: 'Post'
 
   after_save :update_comment_counter
 
   def update_comment_counter
-    author.increment!(:comments_counter)
+    post.update(comments_counter: post.comments.count)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id
 
   validates :name, presence: true
-  validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
   def most_recent_posts
     posts.order(created_at: :desc).limit(3)
   end
