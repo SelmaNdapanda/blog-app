24e445cd0826744bd55e6a954ba2d0fe3baaaa04
Selma <selmandapanda@yahoo.com>
2023-03-03 16:23:47 +0200

    Insert data into tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# first_user = User.create([{name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.'}])
+# second_user = User.create([{ name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.'}])
+
+# Post.create([{author_id: 8, title: 'Hiii', text: 'This is my Second post'}])
+
+# Comment.create([{post_id: 1, author_id: 9, text: 'Hi Tom!'}])
