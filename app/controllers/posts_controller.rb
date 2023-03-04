c111df9af59ba375d47eacc3a9447e6749a3b7c3
Selma <selmandapanda@yahoo.com>
2023-03-04 11:29:34 +0200

    Update post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,30 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    respond_to do |format|
+      format.html do
+        if @post.save
+          flash[:success] = 'Post was successfully created.'
+          redirect_to user_post_path(current_user, @post)
+        else
+          flash.now[:error] = 'Post was not created.'
+          render :new
+        end
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:new_post).permit(:title, :text)
+  end
 end
