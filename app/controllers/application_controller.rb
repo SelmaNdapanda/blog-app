c5eb10ef628005c34f889c35755336be2ca00ea5
Selma <selmandapanda@yahoo.com>
2023-03-07 19:51:01 +0200

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,28 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    User.first
+  before_action :authenticate_user!
+
+  protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  # To redirect users to see all users after sign up
+  def after_sign_up_path_for(_resource)
+    users_path
+  end
+
+  # Determine where to redirect user after successful login.
+  def after_sign_in_path_for(_resource)
+    user_path(current_user)
+  end
+
+  protected
+  
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :photo, :bio, :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :photo, :bio, :email, :password, :password_confirmation, :current_password)
+    end
   end
 end
