c29fc958426bf6be6c6e77a543a925c42e750734
Selma <selmandapanda@yahoo.com>
2023-03-08 10:02:23 +0200

    Configure devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,9 +37,25 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    user_name:     'selmandapanda@yahoo.com',
+    password:      'hamutenyachillimah',
+    domain:        'localhost:3000',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true,
+  }
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
