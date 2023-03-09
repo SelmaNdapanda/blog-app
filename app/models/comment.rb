989b20872dc19167cfbd32b0f76deb2ad5d60e3e
yonas44 <tesfu.yonas1350@gmail.com>
2023-03-09 17:07:33 +0300

    Modify Post and Comment models methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,6 +5,6 @@ class Comment < ApplicationRecord
   after_save :update_comment_counter
 
   def update_comment_counter
-    post.update(comments_counter: post.comments.count)
+    post.increment!(:comments_counter)
   end
 end
