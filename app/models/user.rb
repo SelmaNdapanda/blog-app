ae0964336dc29f9f15c69ab63adac7cdd186ed1f
yonas44 <tesfu.yonas1350@gmail.com>
2023-03-09 17:08:28 +0300

    Add destory method in routes and modify User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  has_many :posts, foreign_key: :author_id
-  has_many :likes, foreign_key: :author_id
-  has_many :comments, foreign_key: :author_id
+  has_many :comments, dependent: :delete_all, foreign_key: 'author_id'
+  has_many :posts, dependent: :delete_all, foreign_key: 'author_id'
+  has_many :likes, dependent: :delete_all, foreign_key: 'author_id'
+
+  ROLES = %i[admin default].freeze
 
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
