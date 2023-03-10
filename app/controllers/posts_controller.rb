dec2d6fd5c9f2a09ee591e5ca4dd3728dbdbcd4e
Selma <selmandapanda@yahoo.com>
2023-03-10 19:10:08 +0200

    Create API endpoint for user's post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,14 @@
 class PostsController < ApplicationController
-  # load_and_authorize_resource
+  load_and_authorize_resource except: [:index, :show]
 
   def index
     @user = User.find(params[:user_id])
     @posts = Post.includes(comments: [:author]).where(posts: { author_id: @user.id })
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => @posts }
+    end
   end
 
   def show
@@ -14,10 +19,10 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
-
+  
   def create
     new_post = current_user.posts.new(post_params)
-
+    
     respond_to do |format|
       format.html do
         if new_post.save
@@ -26,6 +31,8 @@ class PostsController < ApplicationController
           render :new, status: 'Error occured will creating post!'
         end
       end
+      
+      format.json { render :json => @posts }
     end
   end
 
@@ -40,5 +47,13 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:new_post).permit(:title, :text)
+  end
+
+  def renderer(input)
+    respond_to do |format|
+      format.html 
+      format.xml  { render :xml => input }
+      format.json { render :json => input }
+    end
   end
 end
